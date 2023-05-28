c64aab856841e046591c7346a2057338caefd6fe
osy9757 <37167892+osy9757@users.noreply.github.com>
2023-05-28 23:02:30 +0900

    [level 3] Title: 단속카메라, Time: 0.91 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,11 @@
+def solution(routes):
+    routes.sort(key=lambda x: x[1])  
+    last_camera = -30001 
+    answer = 0
+    
+    for route in routes:
+        if last_camera < route[0]:  
+            answer += 1
+            last_camera = route[1]  
+    
+    return answer
