3fb5a615b5956e9a49e8138ff1903edbaf2f3a0e
osy9757 <37167892+osy9757@users.noreply.github.com>
2023-05-28 22:47:10 +0900

    [level 2] Title: 구명보트, Time: 12.32 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    people = deque(people)
+    
+    while people:
+        if len(people) >= 2 and people[0] + people[-1] <= limit:
+            people.popleft()
+        people.pop()
+        answer += 1
+    
+    return answer
