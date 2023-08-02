fa10bb154d9109e6536e9241eed741edc0025350
osy9757 <37167892+osy9757@users.noreply.github.com>
2023-08-03 01:25:47 +0900

    [Gold V] Title: 숫자고르기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.py
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -1,14 +1,3 @@
-def dfs(start, graph, visited, result):
-    loop = []
-    current = start
-    while not visited[current]:
-        visited[current] = True
-        loop.append(current)
-        current = graph[current] - 1
-        if current == start:
-            result += loop
-            break
-
 N = int(input())
 graph = [int(input()) for _ in range(N)]
 
@@ -18,11 +7,17 @@ for i in range(N):
         result.append(i)
         continue
     visited = [False] * N
-    dfs(i, graph, visited, result)
+    loop = []
+    current = i
+    while not visited[current]:
+        visited[current] = True
+        loop.append(current)
+        current = graph[current] - 1
+        if current == i:
+            result += loop
+            break
 
-result = list(set(result))
-result.sort()
-
+result = sorted(set(result))
 print(len(result))
 for number in result:
     print(number + 1)
