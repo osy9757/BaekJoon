7f21bc4677c501b9867e85320e84a32138f38ea7
osy9757 <37167892+osy9757@users.noreply.github.com>
2023-11-07 14:21:49 +0900

    [level 2] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -0,0 +1,12 @@
+def solution(targets):
+    targets.sort(key=lambda x: x[1])
+
+    answer = 0
+    end = -1
+
+    for target in targets:
+        if target[0] >= end:
+            answer += 1
+            end = target[1]
+
+    return answer
