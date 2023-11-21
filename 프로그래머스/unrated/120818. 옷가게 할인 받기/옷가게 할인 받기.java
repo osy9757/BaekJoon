2b9c1d4483eba6d1335abe82620ab0b5821aa648
osy9757 <37167892+osy9757@users.noreply.github.com>
2023-11-22 08:53:43 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int price) {
         if (price >= 500000) 
-            return price * 8 / 10;
+            return (int)(price * 0.8);
         else if (price >= 300000)
-            return price * 9 / 10;
+            return (int)(price * 0.9);
         else if (price >= 100000)
-            return price * 95 / 100;
+            return (int)(price * 0.95);
         else
             return price;
     }
