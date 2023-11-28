5a360795853702996cb7c6f7628c23857b732454
osy9757 <37167892+osy9757@users.noreply.github.com>
2023-11-29 08:47:55 +0900

    [level unrated] Title: 모음 제거, Time: 0.21 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -1,11 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-        String[] vowels = {"a", "e", "i", "o", "u"};
-        
-        for (String v : vowels) {
-            my_string = my_string.replaceAll(v, "");
-        }
-        
-        return my_string;
+        String answer = "";
+        return answer;
     }
 }
