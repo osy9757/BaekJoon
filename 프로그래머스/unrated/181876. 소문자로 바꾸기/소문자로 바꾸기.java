921357b43c581898e2cd7f5656138756fc35e276
osy9757 <37167892+osy9757@users.noreply.github.com>
2023-11-29 09:02:54 +0900

    [level unrated] Title: 소문자로 바꾸기, Time: 9.20 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -1,16 +1,6 @@
 class Solution {
     public String solution(String myString) {
         String answer = "";
-        int num;        
-        
-        for (int i=0; i<myString.length(); i++) {
-            num = (int)myString.charAt(i);
-            if(num<97)
-                answer += (char)(num+32);
-            else
-                answer += (char)(num);
-                
-        }   
         return answer;
     }
 }
